0c89efe0f14bae87e6e43d6f6139f60e974e2ba9
estevaoPagliari <estevao_pagliari@hotmail.com>
2024-08-05 22:00:55 -0300

    geison

--- routes/twilio.ts
+++ routes/twilio.ts
@@ -2,7 +2,30 @@ import { FastifyInstance } from 'fastify'
 import { Twilio } from 'twilio'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
+import { SessionsClient } from '@google-cloud/dialogflow'
+import axios from 'axios'
 
+// Variáveis
+const projectId = 'coliseum-wtpo'
+const verifyToken = 'C0liseum'
+
+// Dialogflow
+const sessionClient = new SessionsClient()
+
+// eslint-disable-next-line camelcase
+const sendMessage = async (to: string, from: string, msg_body: string) => {
+  await axios({
+    method: 'POST',
+    url: `https://graph.facebook.com/v12.0/${to}/messages?access_token=${process.env.WHATSAPP}`,
+    data: {
+      messaging_product: 'whatsapp',
+      to: from,
+      // eslint-disable-next-line camelcase
+      text: { body: msg_body },
+    },
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
 export async function routertwilio(app: FastifyInstance) {
   app.post('/twilio', async (request, reply) => {
     // Substitua os valores de ambiente apropriados
@@ -99,5 +122,111 @@ export async function routertwilio(app: FastifyInstance) {
         error: 'Erro ao enviar mensagem.',
       })
     }
+  })
+
+  app.post('/webhook', async (request, reply) => {
+    // Schema para validação dos query parameters
+    const querySchema = z.object({
+      'hub.mode': z.string(),
+      'hub.verify_token': z.string(),
+      'hub.challenge': z.string(),
+    })
+
+    // Schema para validação do corpo da requisição
+    const bodySchema = z.object({
+      object: z.string(),
+      entry: z.array(
+        z.object({
+          changes: z.array(
+            z.object({
+              value: z.object({
+                metadata: z.object({
+                  phone_number_id: z.string(),
+                }),
+                messages: z.array(
+                  z.object({
+                    from: z.string(),
+                    text: z.object({
+                      body: z.string(),
+                    }),
+                  }),
+                ),
+              }),
+            }),
+          ),
+        }),
+      ),
+    })
+
+    // Verificação e resposta de subscrição
+    const query = querySchema.safeParse(request.query)
+    if (query.success) {
+      const {
+        'hub.mode': mode,
+        'hub.verify_token': token,
+        'hub.challenge': challenge,
+      } = query.data
+
+      if (mode === 'subscribe' && token === verifyToken) {
+        return reply.code(200).send(challenge)
+      } else {
+        return reply.code(403).send('Forbidden')
+      }
+    }
+
+    // Processamento das mensagens
+    const body = bodySchema.safeParse(request.body)
+    if (body.success) {
+      const { entry } = body.data
+      const { changes } = entry[0]
+      const { value } = changes[0]
+      const { metadata, messages } = value
+      const message = messages[0]
+      const to = metadata.phone_number_id
+      const from = message.from
+      // eslint-disable-next-line camelcase
+      const msg_body = message.text.body
+
+      // Definição da sessão do Dialogflow
+      const sessionPath = sessionClient.projectAgentSessionPath(projectId, from)
+      const requestPayload = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            // eslint-disable-next-line camelcase
+            text: msg_body,
+            languageCode: 'pt-BR',
+          },
+        },
+      }
+
+      // Obtendo respostas do Dialogflow
+      try {
+        const [response] = await sessionClient.detectIntent(requestPayload)
+        const queryResult = response?.queryResult
+        if (queryResult) {
+          const fulfillmentMessages = queryResult.fulfillmentMessages
+          if (fulfillmentMessages && fulfillmentMessages.length > 0) {
+            let responseMsg = ''
+            for (const msg of fulfillmentMessages) {
+              if (msg.text && msg.text.text) {
+                responseMsg += msg.text.text.join('\n') + '\n'
+              }
+            }
+            await sendMessage(to, from, responseMsg)
+            return reply.code(200).send()
+          } else {
+            return reply.code(500).send('No fulfillment messages found.')
+          }
+        } else {
+          return reply.code(500).send('No query result found.')
+        }
+      } catch (e) {
+        console.error('Erro ao processar a mensagem:', e)
+        return reply.code(500).send('Erro ao processar a mensagem.')
+      }
+    }
+
+    return reply.code(400).send('Invalid request data')
   })
 }
--- routes/usercliente.ts
+++ routes/usercliente.ts
@@ -1,6 +1,110 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import { z } from 'zod'
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.SENHA,
+  },
+})
+
+// Função para enviar e-mail com HTML
+// Função para enviar e-mail com HTML
+const sendEmail = (to: string, subject: string, senha: string) => {
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Recuperação de Senha</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          margin: 0;
+          padding: 0;
+          background-color: #f4f4f4;
+        }
+        .container {
+          width: 100%;
+          max-width: 600px;
+          margin: 0 auto;
+          background: #ffffff;
+          padding: 20px;
+          border-radius: 5px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+          color: #333333;
+        }
+        p {
+          font-size: 16px;
+          color: #555555;
+        }
+        .button {
+          display: inline-block;
+          font-size: 16px;
+          color: #ffffff;
+          background-color: #007bff;
+          padding: 10px 20px;
+          text-decoration: none;
+          border-radius: 5px;
+          margin-top: 20px;
+        }
+        .footer {
+          font-size: 12px;
+          color: #aaaaaa;
+          margin-top: 20px;
+          text-align: center;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1>Recuperação de Senha</h1>
+        <p>Olá,</p>
+        <p>Recebemos uma solicitação para redefinir a sua senha. Sua nova senha esta aqui:</p>
+        <p>Senha : ${senha}</p>
+        <div class="footer">
+          <p>Atenciosamente,<br>Sua Equipe</p>
+        </div>
+      </div>
+    </body>
+    </html>
+  `
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html: htmlContent,
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Erro ao enviar e-mail:', error)
+    } else {
+      console.log('E-mail enviado:', info.response)
+    }
+  })
+}
+
+const generateRandomPassword = (length: number): string => {
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:',.<>?/"
+  let password = ''
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length)
+    password += charset[randomIndex]
+  }
+  return password
+}
 
 export async function userCliRoutes(app: FastifyInstance) {
   // get geral
@@ -219,6 +323,42 @@ export async function userCliRoutes(app: FastifyInstance) {
 
       // Enviar resposta com o usuário excluído
       return reply.code(200).send(deletedUser)
+    } catch (error) {
+      console.error('Erro ao excluir usuário:', error)
+      // Enviar resposta de erro com código 400
+      return reply.code(400).send({ message: 'Erro ao excluir usuário.' })
+    }
+  })
+
+  app.post('/recuperarsenha', async (request, reply) => {
+    try {
+      const userSchema = z.object({
+        email: z.string().email(), // Validar se é um email válido
+      })
+      const { email } = userSchema.parse(request.body)
+
+      const updateclient = await prisma.userCliente.findMany({
+        where: {
+          email,
+        },
+      })
+
+      if (updateclient.length === 0) {
+        return reply.code(404).send({ message: 'Email não encontrado.' })
+      }
+
+      // Gerar nova senha aleatória
+      const newPassword = generateRandomPassword(10)
+
+      // Atualizar a senha no banco de dados
+      await prisma.userCliente.update({
+        where: { email },
+        data: { senha: newPassword }, // Certifique-se de que o campo 'senha' está sendo corretamente atualizado
+      })
+
+      sendEmail(updateclient[0].email, 'Sua nova senha', newPassword)
+
+      return reply.code(200).send({ message: 'Nova senha enviada por e-mail.' })
     } catch (error) {
       console.error('Erro ao excluir usuário:', error)
       // Enviar resposta de erro com código 400
